75bb764f9af516d30a7087ca4881fb5414549333
Marcelo Mota <eu.nilinho.okra@hotmail.com>
2017-07-29 14:48:42 -0300

    nova interface

--- src/br/uefs/ecomp/SistemaDecolagem/view/ClienteGui.java
+++ src/br/uefs/ecomp/SistemaDecolagem/view/ClienteGui.java
@@ -1,23 +1,22 @@
 package br.uefs.ecomp.SistemaDecolagem.view;
 
+
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.JLabel;
+import javax.swing.JTabbedPane;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
-import javax.swing.LayoutStyle.ComponentPlacement;
-import javax.swing.JTextField;
 import javax.swing.JButton;
-import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JList;
 
 public class ClienteGui extends JFrame {
 
-	private JPanel contentPane;
-	private JTextField origem;
-	private JTextField destino;
+	private JPanel contentPanePrincipal;
 
 	/**
 	 * Launch the application.
@@ -40,144 +39,126 @@ public class ClienteGui extends JFrame {
 	 */
 	public ClienteGui() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 557, 340);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
+		setBounds(100, 100, 908, 376);
+		contentPanePrincipal = new JPanel();
+		contentPanePrincipal.setBorder(new EmptyBorder(0, 0, 0, 0));
+		setContentPane(contentPanePrincipal);
 		
-		JLabel nomeCliente = new JLabel("User");
+		JTabbedPane tabbedPanePrincipal = new JTabbedPane(JTabbedPane.TOP);
 		
-		JLabel lblSelecioneOsTrechos = new JLabel("Selecione os trechos desejados");
-		
-		JLabel lblInsiraAOrigem = new JLabel("Insira a origem");
-		
-		origem = new JTextField();
-		origem.setColumns(10);
-		
-		JLabel lblInsiraODestino = new JLabel("Insira o destino");
-		
-		destino = new JTextField();
-		destino.setColumns(10);
-		
-		JButton btnBuscar = new JButton("Buscar");
-		
-		JComboBox rotasP1 = new JComboBox();
-		
-		JButton btnOKt1 = new JButton("OK");
-		
-		JLabel lblPrimeiroTrecho = new JLabel("Primeiro Trecho");
-		
-		JLabel lblSegundoTrecho = new JLabel("Segundo Trecho");
-		
-		JComboBox rotasP2 = new JComboBox();
-		
-		JButton btnOKt2 = new JButton("OK");
-		
-		JLabel lblTerceiroTrecho = new JLabel("Terceiro Trecho");
-		
-		JComboBox rotasP3 = new JComboBox();
-		
-		JButton btnOkt3 = new JButton("OK");
-		
-		JButton btnFinalizar = new JButton("Finalizar");
-		
-		JLabel lblPosioNaFila = new JLabel("Posi\u00E7\u00E3o na fila de espera");
-		
-		JLabel label = new JLabel("0");
-		GroupLayout gl_contentPane = new GroupLayout(contentPane);
-		gl_contentPane.setHorizontalGroup(
-			gl_contentPane.createParallelGroup(Alignment.TRAILING)
-				.addGroup(gl_contentPane.createSequentialGroup()
+		JLabel lblUser = new JLabel("User");
+		GroupLayout gl_contentPanePrincipal = new GroupLayout(contentPanePrincipal);
+		gl_contentPanePrincipal.setHorizontalGroup(
+			gl_contentPanePrincipal.createParallelGroup(Alignment.LEADING)
+				.addComponent(tabbedPanePrincipal, GroupLayout.DEFAULT_SIZE, 900, Short.MAX_VALUE)
+				.addGroup(gl_contentPanePrincipal.createSequentialGroup()
 					.addContainerGap()
-					.addComponent(lblSelecioneOsTrechos, GroupLayout.DEFAULT_SIZE, 448, Short.MAX_VALUE)
-					.addGap(81))
-				.addGroup(gl_contentPane.createSequentialGroup()
+					.addComponent(lblUser)
+					.addContainerGap(844, Short.MAX_VALUE))
+		);
+		gl_contentPanePrincipal.setVerticalGroup(
+			gl_contentPanePrincipal.createParallelGroup(Alignment.LEADING)
+				.addGroup(Alignment.TRAILING, gl_contentPanePrincipal.createSequentialGroup()
+					.addComponent(lblUser)
+					.addPreferredGap(ComponentPlacement.RELATED, 7, Short.MAX_VALUE)
+					.addComponent(tabbedPanePrincipal, GroupLayout.PREFERRED_SIZE, 328, GroupLayout.PREFERRED_SIZE))
+		);
+		
+		JPanel panelComprarPassagem = new JPanel();
+		tabbedPanePrincipal.addTab("Comprar Passagem", null, panelComprarPassagem, null);
+		
+		JButton btnBuscar = new JButton("buscar");
+		
+		JList listOrigem = new JList();
+		
+		JLabel lblSelecioneAOrigem = new JLabel("Selecione a origem");
+		
+		JLabel lblSelecioneODestino = new JLabel("Selecione o Destino");
+		
+		JList listDestino = new JList();
+		
+		JLabel lblSelecioneOsTrechos = new JLabel("Selecione os trechos");
+		
+		JList listTrechos = new JList();
+		
+		JButton btnComprar = new JButton("Comprar");
+		GroupLayout gl_panelComprarPassagem = new GroupLayout(panelComprarPassagem);
+		gl_panelComprarPassagem.setHorizontalGroup(
+			gl_panelComprarPassagem.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panelComprarPassagem.createSequentialGroup()
 					.addContainerGap()
-					.addComponent(lblPrimeiroTrecho)
-					.addContainerGap(455, Short.MAX_VALUE))
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(lblSegundoTrecho)
-					.addContainerGap(451, Short.MAX_VALUE))
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(lblTerceiroTrecho)
-					.addContainerGap(454, Short.MAX_VALUE))
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addContainerGap()
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
-						.addComponent(rotasP3, Alignment.LEADING, 0, 384, Short.MAX_VALUE)
-						.addComponent(rotasP2, Alignment.LEADING, 0, 384, Short.MAX_VALUE)
-						.addComponent(rotasP1, Alignment.LEADING, 0, 384, Short.MAX_VALUE))
+					.addGroup(gl_panelComprarPassagem.createParallelGroup(Alignment.TRAILING)
+						.addComponent(btnBuscar, Alignment.LEADING)
+						.addGroup(gl_panelComprarPassagem.createSequentialGroup()
+							.addGroup(gl_panelComprarPassagem.createParallelGroup(Alignment.LEADING)
+								.addComponent(listDestino, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 145, Short.MAX_VALUE)
+								.addComponent(lblSelecioneAOrigem)
+								.addComponent(lblSelecioneODestino)
+								.addGroup(gl_panelComprarPassagem.createSequentialGroup()
+									.addComponent(listOrigem, GroupLayout.PREFERRED_SIZE, 145, GroupLayout.PREFERRED_SIZE)
+									.addPreferredGap(ComponentPlacement.RELATED)))
+							.addGroup(gl_panelComprarPassagem.createParallelGroup(Alignment.LEADING)
+								.addGroup(gl_panelComprarPassagem.createSequentialGroup()
+									.addGap(398)
+									.addComponent(lblSelecioneOsTrechos)
+									.addGap(271))
+								.addGroup(Alignment.TRAILING, gl_panelComprarPassagem.createSequentialGroup()
+									.addGap(325)
+									.addGroup(gl_panelComprarPassagem.createParallelGroup(Alignment.TRAILING)
+										.addComponent(btnComprar)
+										.addComponent(listTrechos, GroupLayout.PREFERRED_SIZE, 403, GroupLayout.PREFERRED_SIZE))
+									.addGap(39)))))
+					.addContainerGap())
+		);
+		gl_panelComprarPassagem.setVerticalGroup(
+			gl_panelComprarPassagem.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panelComprarPassagem.createSequentialGroup()
+					.addGap(18)
+					.addGroup(gl_panelComprarPassagem.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblSelecioneAOrigem)
+						.addComponent(lblSelecioneOsTrechos))
 					.addPreferredGap(ComponentPlacement.RELATED)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addComponent(btnOKt1)
-						.addComponent(btnOKt2)
-						.addComponent(btnOkt3))
-					.addGap(90))
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addContainerGap(466, Short.MAX_VALUE)
-					.addComponent(btnFinalizar))
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addComponent(nomeCliente, GroupLayout.PREFERRED_SIZE, 81, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(lblInsiraAOrigem)
-					.addGap(4)
-					.addComponent(origem, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addGroup(gl_panelComprarPassagem.createParallelGroup(Alignment.BASELINE)
+						.addComponent(listOrigem, GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
+						.addComponent(listTrechos, GroupLayout.DEFAULT_SIZE, 25, Short.MAX_VALUE))
+					.addGap(18)
+					.addComponent(lblSelecioneODestino)
 					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addComponent(lblInsiraODestino)
-					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addComponent(destino, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(listDestino, GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
+					.addGap(18)
 					.addComponent(btnBuscar)
-					.addGap(34))
-				.addGroup(Alignment.LEADING, gl_contentPane.createSequentialGroup()
+					.addGap(55)
+					.addComponent(btnComprar)
+					.addGap(53))
+		);
+		panelComprarPassagem.setLayout(gl_panelComprarPassagem);
+		
+		JPanel panelPassagemAdquirida = new JPanel();
+		tabbedPanePrincipal.addTab("Passagens Adquiridas", null, panelPassagemAdquirida, null);
+		
+		JLabel lblPassagemAdquirida = new JLabel("Passagem Adquirida");
+		
+		JList listTrechosAdquiridos = new JList();
+		GroupLayout gl_panelPassagemAdquirida = new GroupLayout(panelPassagemAdquirida);
+		gl_panelPassagemAdquirida.setHorizontalGroup(
+			gl_panelPassagemAdquirida.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panelPassagemAdquirida.createSequentialGroup()
 					.addContainerGap()
-					.addComponent(lblPosioNaFila)
-					.addContainerGap(483, Short.MAX_VALUE))
-				.addGroup(Alignment.LEADING, gl_contentPane.createSequentialGroup()
-					.addGap(43)
-					.addComponent(label)
-					.addContainerGap(450, Short.MAX_VALUE))
+					.addGroup(gl_panelPassagemAdquirida.createParallelGroup(Alignment.LEADING)
+						.addComponent(listTrechosAdquiridos, GroupLayout.PREFERRED_SIZE, 799, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblPassagemAdquirida))
+					.addContainerGap(86, Short.MAX_VALUE))
 		);
-		gl_contentPane.setVerticalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-						.addComponent(nomeCliente, GroupLayout.PREFERRED_SIZE, 31, GroupLayout.PREFERRED_SIZE)
-						.addComponent(lblInsiraAOrigem)
-						.addComponent(origem, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(lblInsiraODestino)
-						.addComponent(destino, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(btnBuscar))
+		gl_panelPassagemAdquirida.setVerticalGroup(
+			gl_panelPassagemAdquirida.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panelPassagemAdquirida.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(lblPassagemAdquirida)
 					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(lblSelecioneOsTrechos)
-					.addGap(2)
-					.addComponent(lblPrimeiroTrecho)
-					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-						.addComponent(rotasP1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(btnOKt1))
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(lblSegundoTrecho)
-					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-						.addComponent(rotasP2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(btnOKt2))
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(lblTerceiroTrecho)
-					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-						.addComponent(rotasP3, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(btnOkt3))
-					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addComponent(btnFinalizar)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(lblPosioNaFila)
-					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addComponent(label)
-					.addContainerGap(15, Short.MAX_VALUE))
+					.addComponent(listTrechosAdquiridos, GroupLayout.PREFERRED_SIZE, 18, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(254, Short.MAX_VALUE))
 		);
-		contentPane.setLayout(gl_contentPane);
+		panelPassagemAdquirida.setLayout(gl_panelPassagemAdquirida);
+		contentPanePrincipal.setLayout(gl_contentPanePrincipal);
 	}
 }
